3ede015c0690439c8ce9ea6faf1b105531f1be3e
Carl Chung <carl.chung@gmail.com>
2023-10-22 17:47:22 +0100

    Initial commit

---
+++ KitchenTimer/KitchenTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenTimerApp.swift
+//  KitchenTimer
+//
+//  Created by carl on 22/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct KitchenTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
